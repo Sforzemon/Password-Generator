1c63fe6c10ca13c261163bd08b15d9eab4ef4ce6
Sforzemon <sforzemon@gmail.com>
2019-12-10 18:20:28 -0500

    Adding initial variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// Prompt the user for what Criteria the password should meet when they press 
+// the generate button.
+// 1st prompt should check for password length 16-128 characters
+// 1st confirm should check for capital letters
+// 2nd confirm should check for lower case letters
+// 3rd confirm should check for numbers
+// 4th comfirm should check for special icons
+// Generate a password that matches the specifications
+// check to validate that the password meets specifications
+// generate another if it doesn't
+// display generated password in text area
+// allow user to be able to copy the password with the clipboard button
+
+var capChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowChar = "abcdefghijklmnopqrstuvwxzy"
+var numChar = "1234567890"
+var specChar = "~!@#$%^&*()_+{}|[];:'/?><,.`"

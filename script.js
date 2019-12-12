f13d897cc16e3936fc6c3f931c68f6d169bea2d5
Sforzemon <sforzemon@gmail.com>
2019-12-12 03:31:28 -0500

    I fixed a bunch of typos, added a screenshot of the page to my images folder, and removed and image I didn't use.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //This psuedo-code is for the original idea, which i branched away from pretty quickly.
 
-
 // Prompt the user for what Criteria the password should meet when they press 
 // the generate button.
 // 1st prompt should check for password length 16-128 characters
@@ -13,7 +12,6 @@
 // generate another if it doesn't
 // display generated password in text area
 // allow user to be able to copy the password with the clipboard button
-
 
 //I added in the use strict because it was keeping me honest about declaring variables
 //I felt I meeded something like that in the beginning.
@@ -55,7 +53,7 @@ passGenBtnPhone.addEventListener("click", passIntro);
 copyClipBtnPhone.addEventListener("click", screenShot);
 
 //I wrote this before I realized I could put functions in functions.
-//I like it though, so I'just kept it to the side.
+//I like it though, so I'll just kept it to the side.
 function followDirections() {
     document.querySelector("#passwordForm").textContent = "";
     document.querySelector("#passwordForm").textContent = "1C4ntf0LL0wD1rect10ns!";
@@ -63,19 +61,19 @@ function followDirections() {
 function itsASpy() {
     document.body.style.display = "none";
 }
-//This stupidly long function is because I wan't to have the secret 
+//This stupidly long function is because I want to have the secret 
 //NSA agent on the other side to seem invested in your choices.  
-//Which means he needs to be able to respond towhatever it is you choose.
+//Which means he needs to be able to respond to whatever it is you choose.
 function passIntro() {
-    //Just establishing variables to be used in this function
+//Just establishing variables to be used in this function
     var newPass = "";
     var newPassArray = [];
     var newPassGen = "";
     var passLength = 0;
     var i = 0;
-    //Yes, this variable is purely there for a joke.
+//Yes, this variable is purely there for a joke.
     var myWindows = 0;
-    //Obviously this is where the function actually begins.
+//Obviously this is where the function actually begins.
     passLength = prompt("Hey there pal. My name is Dave and I am here because I want to help you have a better password.\n\nIdeally for your email, or if you have a Bitcoin wallet, or an Apple ID, really anything.  We are here to help.\n\nFirst thing though, gotta decide how long to make this password.\nThat'll keep the other hackers out.","Type a number between 8 and 128.");
     if (passLength >= 8 && passLength <= 128) {
         var addCaps = confirm("Not a bad number.  Mine is a little longer than that, but we're not here to judge. \n\nOk, lets make this thing, you want capital letters in it?");
@@ -84,12 +82,12 @@ function passIntro() {
             if (addLower === true) {
                 var addNum = confirm("YES, MY DUDE. Keep it going!\nWe gonna sprinkle some numbers up in here?");
                 if (addNum === true) {
-                    var addSym = confirm("I gotta hand it to you, if I wasn't helping you out right now, I would be bummed to have to figure this password out.\n\nYou're doing everything right, my friend, so I gotta ask.\n\nYou want some special symbols? Thats whats really gonna confuse the bad guys out there.");
+                    var addSym = confirm("I gotta hand it to you, if I wasn't helping you out right now, I would be bummed to have to figure this password out.\n\nYou're doing everything right, my friend, so I gotta ask.\n\nYou want some special symbols? That's what's really gonna confuse the bad guys out there.");
                     if (addSym === true) {
                         alert("You have no idea how happy you have made me today. I'm gonna let all the folks in the office know about this.\n\nYou, not the password. That's our little secret.\n\nAnyway, here you go!");
-                        //This seemed like a fun idea until I finished and decided 
-                        //I wanted to be able to validate the passwords...
-                        //Now I have to write a hundred do loops... fun.
+//This seemed like a fun idea until I finished and
+//I still needed to be able to validate the passwords...
+//Now I have to write a hundred do loops... fun.
                         do {
                         var newPass = "";
                         var i = 0;
@@ -178,7 +176,7 @@ function passIntro() {
                         document.querySelector("#passwordForm").textContent = newPassGen;
                     }
                     else {
-                        alert("Big letters and numbers?... Thats it?\n\nWell, I guess I'll see you soon when you need to make a new one.");
+                        alert("Big letters and numbers?... That's it?\n\nWell, I guess I'll see you soon when you need to make a new one.");
                         do {
                             var newPass = "";
                             var i = 0;
@@ -225,13 +223,13 @@ function passIntro() {
                 }
             }
         }
-        //Now start branches if they said no to capital letters
+//Now start branches if they said no to capital letters
         else {
             var addLower = confirm("Ok.. I mean, I get it, but I'm still surprised.\n\nHow about lowercase letters, want those?");
             if (addLower === true) {
                 var addNum = confirm("Nice, I was real worried you were just going to say no to that too.\n\nOk, lets talk numbers.  Want 'em?");
                 if (addNum === true) {
-                    var addSym = confirm("Building steam! We're getting this puppy going! I'm not even gonna waste your time with my innane comments.  I'm so stoked right now that we are in a yes mood, I want to keep this train rolling.  Gotta keep the momentum.\n\nSpecial Character. Yay or nay?");
+                    var addSym = confirm("Building steam! We're getting this puppy going! I'm not even gonna waste your time with my inane comments.  I'm so stoked right now that we are in a yes mood, I want to keep this train rolling.  Gotta keep the momentum.\n\nSpecial Character. Yay or nay?");
                     if (addSym === true) {
                         alert("Oh man, I was worried my rambling might have pushed you to a no.\n\nI don't know how often you hear this, but:\nI'm proud of you, and I'm proud of what we were able to accomplish here today.");
                         do {
@@ -249,7 +247,7 @@ function passIntro() {
                         document.querySelector("#passwordForm").textContent = newPassGen;
                     }
                     else {
-                        myWindows = prompt("I knew I should have just stayed quiet.\n'Just do your job Dave.' They say.\n'You don't even have to say hi to them, just write down their IP and their password.'\nWell, excuse me for trying to bring some joy into this world. Sorry I don't want to be like these souless drones with thier nice big offices, and floor to ceiling windows.\n\nYou know how many windows I have?","Do you?");
+                        myWindows = prompt("I knew I should have just stayed quiet.\n'Just do your job Dave.' They say.\n'You don't even have to say hi to them, just write down their IP and their password.'\nWell, excuse me for trying to bring some joy into this world. Sorry I don't want to be like these soulless drones with thier nice big offices, and floor to ceiling windows.\n\nYou know how many windows I have?","Do you?");
                         if (myWindows > 97 && myWindows < 99) {
                             alert("That... That's right.  How do you know that?  How could you possible have known... unless...\n\nOh shit, you're a spy! I'm outta here!");
                             document.body.style.display = "none";
@@ -275,7 +273,7 @@ function passIntro() {
                 else {
                     var addSym = confirm("Ok, so we at least have lowercase letters... I have the feeling you may be a little simple, but that doesn't mean your password needs to be as well.\n\nWhat do you say? Special characters? For my, apparently very, special little friend?");
                     if (addSym === true) {
-                        alert("Well, at least we got two things in this password.  You succesfully avoided having something like kasjdhflh.\n\nI suppose congratulations are in order.\nJust so you're not waiting, that statement was them.");
+                        alert("Well, at least we got two things in this password.  You successfully avoided having something like kasjdhflh.\n\nI suppose congratulations are in order.\nJust so you're not waiting, that statement was them.");
                         do {
                             var newPass = "";
                             var i = 0;
@@ -302,9 +300,9 @@ function passIntro() {
                     }
                 } 
             }
-            //Now we are on to the no letters at all track
+//Now we are on to the no letters at all track
             else {
-                var addNum = confirm("So, no letters at all? I thought you were just gonna skip the capitals.\n\nYou're never going to remember a password that doesnt have a single letter.\n\nYou're lucky I'm here incase you ever forget it.\n\nSo, we adding numbers or are you skipping that one too?");
+                var addNum = confirm("So, no letters at all? I thought you were just gonna skip the capitals.\n\nYou're never going to remember a password that doesn't have a single letter.\n\nYou're lucky I'm here incase you ever forget it.\n\nSo, we adding numbers or are you skipping that one too?");
                 if (addNum === true) {
                     var addSym = confirm("Ok, cool.  You picked something at least. I feel like I shouldn't congratulate you on such a simple task, and yet I feel compelled because I'm so surprised.\n\nWay to go!\n\nSo what about symbols? You want any of these or are you hoping to generate something that resembles binary?");
                     if (addSym === true) {
@@ -334,9 +332,9 @@ function passIntro() {
                         document.querySelector("#passwordForm").textContent = newPassGen;
                     }
                 }
-                //No Numbers track. Which is, admittedly, pretty short.
+//No Numbers track. Which is, admittedly, pretty short.
                 else {
-                    var addSym = confirm("So let me get this straight:\n\nYou came here. You came to this website, to make a password. And you're not choosing a damn thing to make a password?\n\nYou're choosing symbols right? You couldn't have possibly come here and are going to say no to everything.  Are you?\n\nWhy are you doing this?I have a family to feed, and they will starve if you have a crap password.");
+                    var addSym = confirm("So let me get this straight:\n\nYou came here. You came to this website, to make a password. And you're not choosing a damn thing to make a password?\n\nYou're choosing symbols right? You couldn't have possibly come here and are going to say no to everything.  Are you?\n\nWhy are you doing this? I have a family to feed, and they will starve if you have a crap password.");
                     if (addSym === true) {
                         alert("Thank you.  My children thank you. Little Dave wasn't going to be too happy if he had to skip another meal because daddy couldn't make his quota.\n\nThis means a lot to me.  I won't forget it.\n\n\nYour password that is.  You have been a utter nightmare.");
                         newPass = specChar;
@@ -356,7 +354,7 @@ function passIntro() {
             }
         }
     }
-    //This is for those dummies that cant even pick a number right.
+//This is for those dummies that cant even pick a number right.
     else {
         alert("They were such simple instructions... here just have a password")
         followDirections();
